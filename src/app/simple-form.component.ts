214f6ee6d2a4cbc9ec540669a4ee0c8875361d0d
srinivasan-g <mr.gsrinivasan@gmail.com>
2021-11-30 12:34:00 +0000

    Initial commit

---
+++ src/app/simple-form.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'simple-form',
+  templateUrl: './simple-form.component.html',
+  styleUrls: ['./simple-form.component.css']
+})
+export class SimpleFormComponent {}
