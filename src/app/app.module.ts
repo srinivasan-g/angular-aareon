dfa6187e53dca21a9940a9d4fe452ac0cb847ecf
srinivasan-g <mr.gsrinivasan@gmail.com>
2021-12-02 11:20:45 +0000

    4 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,10 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { SimpleFormComponent } from './simple-form.component';
-import { TitleService } from './title.service';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, ReactiveFormsModule ],
   declarations: [ AppComponent, SimpleFormComponent ],
-  providers: [TitleService],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
