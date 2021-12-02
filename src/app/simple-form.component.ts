9e96d122c29dbec69eadf715389e33a34abe39a7
srinivasan-g <mr.gsrinivasan@gmail.com>
2021-12-02 11:18:35 +0000

    1 file modified

--- src/app/simple-form.component.ts
+++ src/app/simple-form.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { AbstractControl, FormBuilder, FormControl, ValidationErrors, Validators } from '@angular/forms';
-import { Subject} from 'rxjs';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { Subject } from 'rxjs';
 import { takeUntil, tap } from 'rxjs/operators';
 import { Title } from './title.model';
 import { TitleService } from './title.service';
@@ -11,21 +11,22 @@ import { TitleService } from './title.service';
   styleUrls: ['./simple-form.component.css'],
 })
 export class SimpleFormComponent implements OnInit, OnDestroy {
-  
   public titleOptions: Title[] = [];
 
   public submitted: boolean = false;
 
   public form = this._fb.group({
-    title:new FormControl(''),
-    firstName:new FormControl(''),
-    lastName:new FormControl('', Validators.required),
-    acceptForm:new FormControl(false, Validators.requiredTrue)
-  })
+    title: new FormControl(''),
+    firstName: new FormControl(''),
+    lastName: new FormControl('', Validators.required),
+    acceptForm: new FormControl(false, Validators.requiredTrue),
+  });
 
   private readonly destroy$ = new Subject();
 
-  get f() { return this.form.controls; }
+  get f() {
+    return this.form.controls;
+  }
 
   constructor(private _titleService: TitleService, private _fb: FormBuilder) {}
 
@@ -33,39 +34,41 @@ export class SimpleFormComponent implements OnInit, OnDestroy {
     this._titleService
       .getTitles()
       .pipe(
-        tap(
-          (response) => {
-            response = response.filter((obj) => obj.name.match(/[a-z]/i));
-            this.titleOptions = response.sort((a, b) => {
-              const nameA = a.name.toUpperCase(); 
-              const nameB = b.name.toUpperCase();
-              if (nameA < nameB) {
-                return -1;
-              }
-              if (nameA > nameB) {
-                return 1;
-              }
-              return 0;
-            })
-            this.form.get('title')?.patchValue(this.titleOptions.find((item) => item.isDefault));
-          }
-        ),
+        tap((response) => {
+          response = response.filter((obj) => obj.name.match(/[a-z]/i));
+          this.titleOptions = response.sort((a, b) => {
+            const nameA = a.name.toUpperCase();
+            const nameB = b.name.toUpperCase();
+            if (nameA < nameB) {
+              return -1;
+            }
+            if (nameA > nameB) {
+              return 1;
+            }
+            return 0;
+          });
+          this.form
+            .get('title')
+            ?.patchValue(this.titleOptions.find((item) => item.isDefault));
+        }),
         takeUntil(this.destroy$)
       )
       .subscribe();
   }
 
   public invalidLastName(): Boolean {
-    return (this.submitted || this.form.controls['lastName'].touched) && this.form.controls['lastName'].hasError('required');
+    return (
+      (this.submitted || this.form.controls['lastName'].touched) &&
+      this.form.controls['lastName'].hasError('required')
+    );
   }
 
-  public onSubmit(){
+  public onSubmit() {
     this.submitted = true;
-    if(this.form.valid){
-      console.log(this.form.value)
+    if (this.form.valid) {
+      console.log(this.form.value);
     } else {
       return;
-
     }
   }
 
